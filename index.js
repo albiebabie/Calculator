017f474cd0c86d6888cf0c74e1e7c30031229cec
Alban <alban.allen@endclothing.com>
2018-04-13 12:09:27 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Welcome to Alban's fabulous world of chocolate, football and beers!");
+const readline = require("readline-sync");
+
+console.log("Please enter your first number:");
+const firstNumber = parseInt(readline.prompt());
+
+console.log("Please enter your second number:");
+const secondNumber = parseInt(readline.prompt());
+
+const numbersMultiplied = firstNumber * secondNumber;
+
+console.log(numbersMultiplied);
