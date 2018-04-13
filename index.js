4f308af139db1515b3cdfa48645950cd2922846c
Alban <alban.allen@endclothing.com>
2018-04-13 12:52:28 +0100

    Calculator - accepts operators - switch

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const readline = require("readline-sync");
 
+console.log("Please chose from the following operators: \n" +
+    "+ - * / \n" +
+    "then type your selection.");
+
+const selectedOperator = readline.prompt();
+var finalNumber;
+
 console.log("Please enter your first number:");
 const firstNumber = parseInt(readline.prompt());
 
 console.log("Please enter your second number:");
 const secondNumber = parseInt(readline.prompt());
 
-const numbersMultiplied = firstNumber * secondNumber;
+switch (selectedOperator) {
+    case "+":
+        console.log(selectedOperator);
+        finalNumber = firstNumber + secondNumber;
+        break;
 
-console.log(numbersMultiplied);
+    case "-":
+        console.log(selectedOperator);
+        finalNumber = firstNumber - secondNumber;
+        break;
+
+    case "*":
+        console.log(selectedOperator);
+        finalNumber = firstNumber * secondNumber;
+        break;
+
+    case "/":
+        console.log(selectedOperator);
+        finalNumber = firstNumber / secondNumber;
+        break;
+
+    default:
+        console.log("you haven't selected an operator!");
+        break;
+}
+
+console.log(finalNumber);
